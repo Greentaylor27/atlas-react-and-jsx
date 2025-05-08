126f0a37eca91babfd01d418d51a87d2cfabf30a
Greentaylor27 <greentaylor18@gmail.com>
2025-05-08 13:40:15 -0500

    Adding SocialLink component

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,12 @@
+export default function SocialLinks() {
+    return (
+        <div className="social-link">
+            <a href="https://www.linkedin.com/in/greentaylor27/" target="_blank" rel="noopener noreferrer">
+                <img src="src/assets/linkedin.svg" alt="Linkedin" />
+            </a>
+            <a href="https://github.com/Greentaylor27" target="_blank" rel="noopener noreferrer">
+                <img src="src/assets/github.svg" alt="Github" />
+            </a>
+        </div>
+    )
+}
