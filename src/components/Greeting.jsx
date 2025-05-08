79bfe6bdd330b50e212ffefee2f6ba8fd97e6f23
Greentaylor27 <greentaylor18@gmail.com>
2025-05-08 13:24:20 -0500

    I believe Greeting component is complete

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -1,3 +1,23 @@
 export default function Greeting() {
-    
+    const time = new Date().getHours();
+    let message = 'Good Night!';
+    let img = 'src/assets/night.svg';
+
+    if (time >= 6 && time < 12) {
+        message = 'Good Morning!';
+        img = 'src/assets/day.svg';
+    } else if (time >= 12 && time < 17) {
+        message = 'Good Afternoon!';
+        img = 'src/assets/day.svg'
+    } else if (time >= 17 && time < 21) {
+        message = 'Good Evening!';
+        img = 'src/assets/evening.svg';
+    }
+
+    return (
+        <h1 className="greeting">
+            <img src={img} />
+            {message}
+        </h1>
+    )
 }
